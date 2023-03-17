1d77f2311a07daa95f13946a02f884fc128a32ef
ouiam895 <ouiam.charki@enim.ac.ma>
2023-03-17 09:01:24 -0700

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
-/**
- * main -A program that prints a line with a puts function.
- * Return : 0 (Success)
+/*
+ * Main -A program that prints a line with a puts function.
+ *
+ * Return :Always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /*
- * main -A programm that prints a line using printf.
- * Return : 0
+ * Main -A programm that prints a line using printf.
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
