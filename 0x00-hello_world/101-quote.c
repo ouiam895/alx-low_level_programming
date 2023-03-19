00ca6def409c0da0fab2b0c27301833299baf5bc
ouiam895 <ouiam.charki@enim.ac.ma>
2023-03-19 04:11:18 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: 1 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
